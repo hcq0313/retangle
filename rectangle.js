f1cc7ee77b251437f5e6a1447f7f2d25b871888b
hcq0313 <853801007@qq.com>
2019-03-15 14:10:03 +0800

    添加了css样式

--- rectangle.js
+++ rectangle.js
@@ -5,14 +5,14 @@ $(function(){
       $perimeter = $('#perimeter'),
       $area = $('#arear');
 
-  $btnCal.click(function({
-    var w = Number($width),
-        h = Number($height);
+  $btnCal.click(function(){
+    var w = Number($width.val()),
+        h = Number($height.val());
 
   var p = 2*(w+h),
       a = w*h;
 
-  $perimeter.val();
-  $area.val();
-  }));
+  $perimeter.val(p);
+  $area.val(a);
+  });
 });
